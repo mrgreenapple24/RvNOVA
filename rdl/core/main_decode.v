5ce5f053dd4364dfa2fbd0abc9b9a30e3d432260
mrgreenapple24 <mrchill2405@gmail.com>
2026-01-08 09:39:51 +0530

    main decoder fully completed

--- rdl/core/main_decode.v
+++ rdl/core/main_decode.v
@@ -4,10 +4,11 @@ module main_decode(
     output alu_src, // second operand reg(0) or imm(1)
     output mem_write, // writing into memory
     output mem_read, // reading from memory
-    output mem_to_reg, // data written to reg come from mem or alu
+    output mem_to_reg[1:0], // 00: ALU, 01: memory, 10: PC+4
     output alu_op[2:0], // tells ALU decoder what type of instr it is
     output branch, // PC changes
     output jump // PC changes
+    output op1_src // rs1 or PC used
 );
 
 always @(*) begin
@@ -16,10 +17,11 @@ reg_write  = 0;
 alu_src    = 0;
 mem_write  = 0;
 mem_read   = 0;
-mem_to_reg = 0;
+mem_to_reg = 2'b00;
 alu_op     = 3'b000;
 branch     = 0;
 jump       = 0;
+op1_src = 0;
 
 case (instr[6:2]) // Opcode
 
@@ -36,7 +38,7 @@ case (instr[6:2]) // Opcode
         alu_src = 1;
         reg_write = 1;
         mem_read = 1;
-        mem_to_reg = 1;
+        mem_to_reg = 2'b01;
         alu_op = 3'b000;
     end
     5'b01000: begin // S-type
@@ -49,17 +51,20 @@ case (instr[6:2]) // Opcode
         alu_src = 1;
         alu_op = 3'b001;
     end
-    5'b11011: begin // J-type
+    5'b11011: begin // JAL
         jump = 1;
-        alu_src = 1;
-        alu_op = 3'b000;
         reg_write = 1;
+        alu_src = 1;
+        op1_src = 1;
+        mem_to_reg = 2'b10;
+        alu_op = 3'b000;
     end
-    5'b11001: begin // I-type
+    5'b11001: begin // JALR
         jump = 1;
         alu_src = 1;
-        alu_op = 3'b000;
+        mem_to_reg = 2'b10;
         reg_write = 1;
+        alu_op = 3'b000;
     end
     5'b01101: begin // U-type
         alu_src = 1;
@@ -69,13 +74,21 @@ case (instr[6:2]) // Opcode
     5'b00101: begin // U-type
         alu_src = 1;
         reg_write = 1;
-        alu_op = 3'b101;
+        op1_src = 1;
+        alu_op = 3'b000;
     end
-    5'b11100: begin // I-type
-        // Transfer control to OS
-    end
-    5'b11100: begin // I-type
-        // Transfer control to debugger
+    5'b11100: begin // ecall/ebreak
+        if (instr[14:12] == 3'b000) begin
+            if (instr[20] == 0) begin
+                is_ecall  = 1;
+            end else begin
+                is_ebreak = 1;
+            end
+            end 
+            else begin
+                csr_write = 1; 
+                reg_write = 1;
+            end
     end
 
     default: $display("Opcode not recognized");
